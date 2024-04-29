7d0c5fa7495d465136a4c4f5f73344d40399a637
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-04-29 17:10:20 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [".src/index.html", "./src/App.jsx", "./src/main.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
